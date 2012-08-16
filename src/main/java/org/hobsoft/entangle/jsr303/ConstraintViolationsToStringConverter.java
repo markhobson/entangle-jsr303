b227b1da77ae2a9deaabd030c9c0039ef9dd214b
Mark Hobson <markhobson@gmail.com>
2012-08-16 20:52:00 +0100

    Renamed package to org.hobsoft.entangle.jsr303

--- src/main/java/org/hobsoft/entangle/jsr303/ConstraintViolationsToStringConverter.java
+++ src/main/java/org/hobsoft/entangle/jsr303/ConstraintViolationsToStringConverter.java
@@ -3,7 +3,7 @@
  * 
  * (c) 2011 IIZUKA Software Technologies Ltd.  All rights reserved.
  */
-package uk.co.iizuka.common.binding.jsr303;
+package org.hobsoft.entangle.jsr303;
 
 import java.util.Collection;
 
